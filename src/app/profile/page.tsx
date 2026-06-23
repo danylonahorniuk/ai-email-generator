673a2bb49f3b07f97ee9d90c3fb7f57e26c549ec
danylonahorniuk <danylo.nahorniuk@gmail.com>
2026-06-23 23:21:58 +0300

    fix: increase profile page max-width to 1400px for wide screens

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,7 +12,7 @@ export default async function ProfilePage() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar user={user} />
-      <main className="flex-1 mx-auto w-full max-w-5xl px-4 sm:px-6 lg:px-8 py-10">
+      <main className="flex-1 mx-auto w-full max-w-[1400px] px-4 sm:px-6 lg:px-8 py-10">
         <ProfileClient user={{ id: user.id, email: user.email ?? '', created_at: user.created_at }} />
       </main>
     </div>
